56d2b4058e7433c586b62dd7acc54b032b41ae56
SRIDHAR RAO KUKKALA <50567090+iSRIDHARRAO@users.noreply.github.com>
2021-04-30 20:10:07 +0530

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 read -s -p "Enter terminal password: " password
 echo
-sed "/s/admin/$password/g" loginbash.sh > login12
+sed "s/admin/$password/g" loginbash.sh > login12
 rm loginbash.sh
 sudo chmod +x login12
 sudo echo PATH=$PATH:`pwd` >> $HOME/.bashrc
